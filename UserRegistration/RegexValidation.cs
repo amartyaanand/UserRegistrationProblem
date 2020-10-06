8c799e9d163d816608c73ef1230481eb0aa1822f
Amartya <amartyaanand21@live.com>
2020-10-06 22:43:31 +0530

    [Amartya] Add : Validating Email.

--- UserRegistration/RegexValidation.cs
+++ UserRegistration/RegexValidation.cs
@@ -9,6 +9,7 @@ namespace UserRegistration
     {
         public const string Regex_FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
         public const string Regex_LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string Regex_Email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, Regex_FirstName);
@@ -17,6 +18,9 @@ namespace UserRegistration
         {
             return Regex.IsMatch(lastName, Regex_LastName);
         }
+        public bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, Regex_Email);
+        }
     }
 }
-
