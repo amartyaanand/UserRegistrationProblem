9dfd5ee2f3c0d3843b41984461e3055cb492a46d
Amartya <amartyaanand21@live.com>
2020-10-12 22:32:47 +0530

    [Amartya] Add : Clearing all email samples.

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -22,6 +22,31 @@ namespace UserRegistration
             Console.WriteLine("Enter Your Password");
             string password = Console.ReadLine();
             Console.WriteLine(regexValidation.ValidatePassword(password));
+
+            //Validating Sample Emails
+            List<string> sampleEmailList = new List<string>()
+            {
+                "abc@yahoo.com",
+                "abc-100@yahoo.com",
+                "abc.100@yahoo.com",
+                "abc111@abc.com",
+                "abc-100@abc.net",
+                "abc.100@abc.com.au",
+                "abc@1.com",
+                "abc@gmail.com.com",
+                "abc+100@gmail.com"
+            };
+            foreach (string item in sampleEmailList)
+            {
+                if (regexValidation.ValidateEmail(item))
+                {
+                    Console.WriteLine("Valid Email");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Email");
+                }
+            }
         }
     }
 }
