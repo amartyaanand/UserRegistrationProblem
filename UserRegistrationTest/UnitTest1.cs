4c7ee583968f1abb04187a8ee0e20f71e3da791c
Amartya <amartyaanand21@live.com>
2020-10-12 23:01:58 +0530

    [Amartya] Add : Custom Exception on invalid user details.

--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.Serialization;
 using UserRegistration;
 
 namespace UserRegistrationTest
@@ -63,49 +65,69 @@ namespace UserRegistrationTest
         }
         //Sad Test Cases(Test Cases Fail The Entry)
         [TestMethod]
-        public void GivenUserFistName_WhenValidate_ShouldReturnFalse()
+        public void GivenUserFistName_WhenValidate_Should_throw_Custom_InvalidFirstNameException()
         {
-            string firstName = "swapnil";
-            //Act
-            bool result = user.ValidateFirstName(firstName);
-            //Assert
-            Assert.IsFalse(result);
+            try
+            {
+                string firstName = "swapnil";
+                bool result = user.ValidateFirstName(firstName);
+            }
+            catch (InvalidUserDetailException e)
+            {
+                Assert.AreEqual("Invalid First Name", e.Message);
+            }
         }
         [TestMethod]
-        public void GivenUserLastName_WhenValidate_ShouldReturnFalse()
+        public void GivenUserLastName_WhenValidate_Should_throw_Custom_InvalidLastNameException()
         {
-            string lastName = "chakravarti";
-            //Act
-            bool result = user.ValidateFirstName(lastName);
-            //Assert
-            Assert.IsFalse(result);
+            try
+            {
+                string lastName = "chakravarti";
+                bool result = user.ValidateLastName(lastName);
+            }
+            catch (InvalidUserDetailException e)
+            {
+                Assert.AreEqual("Invalid Last Name", e.Message);
+            }
         }
         [TestMethod]
-        public void GivenEmailId_WhenValidate_ShouldReturnFalse()
+        public void GivenEmailId_WhenValidate_Should_throw_Custom_InvalidEmailException()
         {
-            string email = "Swapnil.Chakravarti.com";
-            //Act
-            bool result = user.ValidateEmail(email);
-            //Assert
-            Assert.IsFalse(result);
+            try
+            {
+                string email = "Swapnil.Chakravarti.com";
+                bool result = user.ValidateEmail(email);
+            }
+            catch (InvalidUserDetailException e)
+            {
+                Assert.AreEqual("Invalid Email", e.Message);
+            }
         }
         [TestMethod]
-        public void GivenMobileNumber_WhenValidate_ShouldReturnFalse()
+        public void GivenMobileNumber_WhenValidate_Should_throw_Custom_InvalidMobileException()
         {
-            string mobileNumber = "91 0876543210";
-            //Act
-            bool result = user.ValidateMobile(mobileNumber);
-            //Assert
-            Assert.IsFalse(result);
+            try
+            {
+                string mobileNumber = "91 0876543210";
+                bool result = user.ValidateMobile(mobileNumber);
+            }
+            catch (InvalidUserDetailException e)
+            {
+                Assert.AreEqual("Invalid Mobile Number", e.Message);
+            }
         }
         [TestMethod]
-        public void GivenPassword_WhenValidate_ShouldReturnFalse()
+        public void GivenPassword_WhenValidate_Should_throw_Custom_InvalidPasswordException()
         {
-            string password = "swapnil@123*";
-            //Act
-            bool result = user.ValidatePassword(password);
-            //Assert
-            Assert.IsFalse(result);
+            try
+            {
+                string password = "swapnil@123*";
+                bool result = user.ValidatePassword(password);
+            }
+            catch (InvalidUserDetailException e)
+            {
+                Assert.AreEqual("Invalid Password", e.Message);
+            }
         }
         [TestMethod]
         [DataRow("abc@yahoo.com")]
@@ -122,4 +144,25 @@ namespace UserRegistrationTest
             Assert.IsTrue(user.ValidateEmail(email));
         }
     }
+
+    [Serializable]
+    internal class InvalidUserDetailException : Exception
+    {
+        public InvalidUserDetailException()
+        {
+        }
+
+        public InvalidUserDetailException(string message) : base(message)
+        {
+        }
+
+        public InvalidUserDetailException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidUserDetailException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
 }
+
